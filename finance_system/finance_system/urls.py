5db931f347ee89a79927b7e7c4c7d78f0c3b0ac4
Kaustubh Mishra <kaustubhmanimishra.s2@gmail.com>
2026-04-03 17:37:34 +0530

    Add home page UI function
    Added a basic home page UI function to handle requests.

--- finance_system/finance_system/urls.py
+++ finance_system/finance_system/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
+# for basic home page UI
 def home(request):
     return HttpResponse("""
     <!DOCTYPE html>
